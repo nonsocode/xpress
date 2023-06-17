b35f219b83531cadbf697162b71f945a21f32568
Chinonso Chukwuogor <nonsochukwuogor@gmail.com>
2023-06-17 11:31:43 -0700

    action

--- main.go
+++ main.go
@@ -11,12 +11,20 @@ func main() {
 	evaluator := *parser.NewInterpreter()
 	template1 := "hehe{{-123 * (45.67) }}"
 	template2 := `{{ 3 * 3 }} withot spaces is {{ true ? "changed" : "not changed" }}`
-	template3 := `This year's woman of the year is {{ hello().woman.of[0] }}`
+	template3 := `This year's woman of the year is {{ hello().woman.funcs().justice }}`
 	evaluator.AddFunc("hello", func(args ...interface{}) (interface{}, error) {
 		return map[string]interface{}{
 			"woman": map[string]interface{}{
 				"of": []interface{}{
 					2020, 2021, 2022,
+				},
+				"funcs": func(args ...interface{}) (interface{}, error) {
+					type HMap struct {
+						justice string
+					}
+					return HMap{
+						justice: "Ruth Bader Ginsburg",
+					}, nil
 				},
 			},
 		}, nil
